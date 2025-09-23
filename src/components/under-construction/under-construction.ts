2ad1bc8e9aa1822b0fe30fee13f8ebec3095d955
Ilker Kamis <ilker.kamis@Vault.Insurance>
2025-09-23 18:06:54 +0300

    wip

--- src/components/under-construction/under-construction.ts
+++ src/components/under-construction/under-construction.ts
@@ -53,7 +53,7 @@ export class UnderConstruction extends HTMLElement {
             </div>
           </div>
 
-          <a class="back-btn" href="/">
+          <a class="back-btn" href="/ilkerCoder/">
             <i class="fas fa-arrow-left"></i>
             <span>Geri Dön</span>
           </a>
