d7e94e4ea1b27502519323228afd966c197c110b
Ilker Kamis <ilker.kamis@Vault.Insurance>
2025-09-23 12:13:59 +0300

    wip

---
+++ src/components/header/header.ts
@@ -0,0 +1,53 @@
+import "./header.scss";
+
+export class Header extends HTMLElement { 
+    connectedCallback(){
+        this.innerHTML = `<header class="main-header is--container">
+
+  <!-- Logo / İsim -->
+  <a href="/" class="main-header__title is--fancyfont">
+    <h1 class="main-header__h1 is--nomargin">Test</h1>
+  </a>
+
+  <!-- Navigasyon -->
+<nav class="main-navigation">
+  <ul class="main-navigation__list is--nolist is--nomargin">
+    <li class="main-navigation__entry">
+      <a href="/" class="main-navigation__link main-navigation__link--active">
+        About
+        <svg width="50" viewBox="0 0 173.5 30.9" xmlns="http://www.w3.org/2000/svg">
+          <path d="M173 5c-1-4-6-3-7-3l-5 2-15 3-7-2c-5-3-12-5-19-4-8 1-13 6-17 9l-6 4-7-5c-4-3-8-7-15-8-13-3-20 3-24 8-2 2-3 3-5 3-3 0-5-1-8-3-4-3-9-7-17-8S6 3 2 10c-1 2-4 7-1 10 2 3 5 3 6 3 3 0 5-2 6-4l1-1 1-1c2-2 3-4 8-1l4 2c5 4 13 9 20 10 9 0 13-5 16-8 2-3 3-4 7-3s7 3 9 6c4 3 8 8 16 8l7-1c10-2 13-7 16-11 3-3 3-4 10-1 18 7 25 3 34-3l6-3c3-1 7-3 5-7Z" fill="currentColor"></path>
+        </svg>
+      </a>
+    </li>
+    <li class="main-navigation__entry">
+      <a href="/blog/" class="main-navigation__link">
+        Blog
+        <svg width="50" viewBox="0 0 173.5 30.9" xmlns="http://www.w3.org/2000/svg">
+          <path d="M173 5c-1-4-6-3-7-3l-5 2-15 3-7-2c-5-3-12-5-19-4-8 1-13 6-17 9l-6 4-7-5c-4-3-8-7-15-8-13-3-20 3-24 8-2 2-3 3-5 3-3 0-5-1-8-3-4-3-9-7-17-8S6 3 2 10c-1 2-4 7-1 10 2 3 5 3 6 3 3 0 5-2 6-4l1-1 1-1c2-2 3-4 8-1l4 2c5 4 13 9 20 10 9 0 13-5 16-8 2-3 3-4 7-3s7 3 9 6c4 3 8 8 16 8l7-1c10-2 13-7 16-11 3-3 3-4 10-1 18 7 25 3 34-3l6-3c3-1 7-3 5-7Z" fill="currentColor"></path>
+        </svg>
+      </a>
+    </li>
+    <li class="main-navigation__entry">
+      <a href="/demo/" class="main-navigation__link">
+        Demo
+        <svg width="50" viewBox="0 0 173.5 30.9" xmlns="http://www.w3.org/2000/svg">
+          <path d="M173 5c-1-4-6-3-7-3l-5 2-15 3-7-2c-5-3-12-5-19-4-8 1-13 6-17 9l-6 4-7-5c-4-3-8-7-15-8-13-3-20 3-24 8-2 2-3 3-5 3-3 0-5-1-8-3-4-3-9-7-17-8S6 3 2 10c-1 2-4 7-1 10 2 3 5 3 6 3 3 0 5-2 6-4l1-1 1-1c2-2 3-4 8-1l4 2c5 4 13 9 20 10 9 0 13-5 16-8 2-3 3-4 7-3s7 3 9 6c4 3 8 8 16 8l7-1c10-2 13-7 16-11 3-3 3-4 10-1 18 7 25 3 34-3l6-3c3-1 7-3 5-7Z" fill="currentColor"></path>
+        </svg>
+      </a>
+    </li>
+    <li class="main-navigation__entry">
+      <a href="/art/" class="main-navigation__link">
+        Art
+        <svg width="50" viewBox="0 0 173.5 30.9" xmlns="http://www.w3.org/2000/svg">
+          <path d="M173 5c-1-4-6-3-7-3l-5 2-15 3-7-2c-5-3-12-5-19-4-8 1-13 6-17 9l-6 4-7-5c-4-3-8-7-15-8-13-3-20 3-24 8-2 2-3 3-5 3-3 0-5-1-8-3-4-3-9-7-17-8S6 3 2 10c-1 2-4 7-1 10 2 3 5 3 6 3 3 0 5-2 6-4l1-1 1-1c2-2 3-4 8-1l4 2c5 4 13 9 20 10 9 0 13-5 16-8 2-3 3-4 7-3s7 3 9 6c4 3 8 8 16 8l7-1c10-2 13-7 16-11 3-3 3-4 10-1 18 7 25 3 34-3l6-3c3-1 7-3 5-7Z" fill="currentColor"></path>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+</header>`
+    }
+}
+customElements.define("me-header" , Header);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Header } from "./components/header/header";
+import { Avatar } from "./components/avatar/avatar";
+
+
 document.querySelectorAll("polygon").forEach(poly => {
   poly.addEventListener("mouseenter", () => {
     poly.classList.add("animate");
 
-    // animasyon bittikten sonra sınıfı sil (tekrar çalışabilsin)
     poly.addEventListener("animationend", () => {
       poly.classList.remove("animate");
     }, { once: true });
